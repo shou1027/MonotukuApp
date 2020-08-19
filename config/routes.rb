145ae133cea7c94a92ca7263e3ce0992f2da3c65
shou1027 <longword871@gmail.com>
2020-08-19 17:32:33 +0000

    Update routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,28 @@
 Rails.application.routes.draw do
-  get 'users/index'
-
-  get 'users/login_form'
-
-  get 'users/show'
-
-  get 'users/signup'
+  get 'posts/index'
 
   get 'posts/edit'
 
+  get 'posts/post'
+
   get 'posts/search'
 
-  get 'home/about'
+  get 'users/show'
 
-  get 'home/help'
-
-  get 'home/home'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'users/index'
+  
+  get "signup" => "users#signup"
+  get "login" => "users#login_form"
+  post "login" => "users#login"
+  get "logout" => "users#logout"
+  post "new" => "users#new"
   
   get "/" => "home#home"
+  get "timeline" => "home#timeline"
+  get "tag" => "home#tag"
+  get "about" => "home#about"
+  get "help" => "home#help"
+  
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
