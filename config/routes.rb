964d7601d6c7e455a3ecf2bab8794f84978529c9
shou1027 <longword871@gmail.com>
2020-08-19 17:18:57 +0000

    Test deploy

--- config/routes.rb
+++ config/routes.rb
@@ -19,5 +19,5 @@ Rails.application.routes.draw do
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   
-  get "/" => "application#hello"
+  get "/" => "users/index"
 end
