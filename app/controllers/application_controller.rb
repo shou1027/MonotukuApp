d79692da6fe483eb8a6f96b78f1569c130589991
shou1027 <longword871@gmail.com>
2020-08-19 17:28:28 +0000

    Update application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,27 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_current_user
   
-  def hello
-    render html: "hello"
-  end
+  private
+    def set_current_user
+      @current_user = session[:user_name]
+    end
+    
+    def allow_logged_in
+      if !@current_user
+        redirect_to("/about")
+      end
+    end
+    
+    def allow_logged_out
+      if @current_user
+        redirect_to("/")
+      end
+    end
+    
+    def allow_proper_user
+      if @current_user != params[:user_name]
+        redirect_to("/")
+      end
+    end
 end
