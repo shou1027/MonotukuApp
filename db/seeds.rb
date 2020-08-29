ea98e4943d67ea91fd8eae35439997279ce8f729
shou1027 <longword871@gmail.com>
2020-08-29 12:25:45 +0000

    Update files

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 100.times do |n|
-  User.create(name: (0...12).map{ ('A'..'Z').to_a[rand(26)] }.join, password: (0...8).map{ ('A'..'Z').to_a[rand(26)] }.join, image_name: "default.jpg")
+  User.create(name: (0...12).map{ ('a'..'z').to_a[rand(26)] }.join, password: (0...8).map{ ('A'..'Z').to_a[rand(26)] }.join, image_name: "default.jpg")
 end
 
 users = User.all
